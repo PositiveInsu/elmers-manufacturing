40558a98770cf3b47889eb7fa380af299aca3899
PositiveInsu <positiveinsu@gmail.com>
2023-04-28 06:28:37 -0500

    Initialize HexadecimalToProperty api test code

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'api'], function(){
     Route::get('/public-function/digit-to-word/{locale}/{digit}', [PublicFunctionController::class, 'digitToWord']);
+    Route::get('/public-function/hexdecimal-to-property/{hexadecimal}', [PublicFunctionController::class, 'hexadecimalToProperty']);
 });
